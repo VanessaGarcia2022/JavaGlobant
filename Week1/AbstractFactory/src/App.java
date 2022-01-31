72e54b9e63bc555d2ef50a793560c793b55621d0
vanessa.garcia <vanessa.garcia@globant.com>
2022-01-30 20:14:16 -0500

    notes and excercises of Abtrac factory desing pattern

--- Week1/AbstractFactory/src/App.java
+++ Week1/AbstractFactory/src/App.java
@@ -1,7 +1,18 @@
 import interfaz.AbstractFactory;
+import interfaz.IConnexionBD;
+import interfaz.IConnexionREST;
 
 public class App {
     public static void main(String[] args) {
-        //AbstractFactory fabricaBD = Fa
+        AbstractFactory fabricaBD = FabricaProductor.getFactory("BD");
+        IConnexionBD cxBD1 = fabricaBD.getBD("MYSQL");
+
+        cxBD1.conectar();
+
+        AbstractFactory fabricaREST = FabricaProductor.getFactory("REST");
+        IConnexionREST cxRS1 = fabricaREST.getREST("COMPRAS");
+
+        cxRS1.leerURL("https://www.youtube.com/subscription_center?add_user=mitocode");
+
     }
 }
