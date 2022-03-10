c15e26faf4c7dc298dd7b2a36c36b6a3229482d4
vanessa.garcia <vanessa.garcia@globant.com>
2022-03-10 15:42:17 -0500

    other files

---
+++ PersonalPractice/spring5webapp/spring5webapp/src/test/java/vanessa/springframework/spring5webapp/Spring5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package vanessa.springframework.spring5webapp;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testng.annotations.Test;
+
+@SpringBootTest
+class Spring5webappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
