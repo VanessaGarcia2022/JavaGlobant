dd2aff4669321fdca5306ced43441c5c0d10ab4f
vanessa.garcia <vanessa.garcia@globant.com>
2022-03-31 15:46:51 -0500

    Final Work

---
+++ FinalWork/email/src/main/java/com/vanessadev/emailProject/EmailApplication.java
@@ -0,0 +1,13 @@
+package com.vanessadev.emailProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailApplication.class, args);
+	}
+
+}
